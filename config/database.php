6720494626e2cac4ad74956e7aa11d6fac29effd
albimdkr <albimudakar04@gmail.com>
2024-04-27 22:00:03 +0700

    ready to prod

--- config/database.php
+++ config/database.php
@@ -5,12 +5,6 @@ $username = "root";
 $password = "";
 $database = "db_siswa";
 
-// env: prod
-// $host     = "sql202.infinityfree.com";            
-// $username = "if0_36442778";
-// $password = "3xfBvUoqwbJz";
-// $database = "if0_36442778_db_siswa";
-
 // buat koneksi database
 $mysqli = new mysqli($host, $username, $password, $database);
 
