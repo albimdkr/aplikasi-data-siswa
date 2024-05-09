7310469982944ee8650c5002ee3019dce1e9149f
albimdkr <albimudakar04@gmail.com>
2024-05-09 11:17:05 +0700

    feat: adding message issue

--- config/database.php
+++ config/database.php
@@ -5,11 +5,11 @@
 // $password = "";
 // $database = "db_siswa";
 
-// env: prod
-$host     = "sql101.infinityfree.com";            
-$username = "if0_36443573";
-$password = "hueEksiFvXwMeUP";
-$database = "if0_36443573_db_siswa";
+// // env: prod (having issue on local vm)
+// $host     = "sql101.infinityfree.com";            
+// $username = "if0_36443573";
+// $password = "hueEksiFvXwMeUP";
+// $database = "if0_36443573_db_siswa";
 
 // buat koneksi database
 $mysqli = new mysqli($host, $username, $password, $database);
